eeb4ceba0275b15f662e0aca1737f4627e1aabbc
huangjinwei <zsjinwei@foxmail.com>
2025-07-20 11:16:58 +0800

    add 256MB support

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,4 @@ sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/HJW-Router/g' package/base-files/files/bin/config_generate
